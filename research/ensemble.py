09dedb12048da97d6d6309cc414f5d91ba373ffb
Leonardo Raggio <leonardo.raggio@gmail.com>
2022-08-09 14:29:55 +0000

    ensemble RF and LGBM

--- research/ensemble.py
+++ research/ensemble.py
@@ -4,8 +4,11 @@ import pandas as pd
 
 project_root = Path(__file__).parents[1]
 
-def ensemble() -> pd.DataFrame:
-    seed42 = pd.read_csv(project_root / "data/Predictions/test_lgbm_dart_baseline_5fold_seed42.csv")
+
+def ensemble_lgbm() -> pd.DataFrame:
+    seed42 = pd.read_csv(
+        project_root / "data/Predictions/test_lgbm_dart_baseline_5fold_seed42.csv"
+    )
     seed2022 = pd.read_csv(
         project_root / "data/Predictions/test_lgbm_dart_baseline_5fold_seed2022.csv"
     )
@@ -17,8 +20,29 @@ def ensemble() -> pd.DataFrame:
     return predictions
 
 
+def ensemble() -> pd.DataFrame:
+    lgbm_score = 0.799
+    rf_score = 0.77
+    lgbm_weight = lgbm_score / (lgbm_score + rf_score)
+    rf_weight = rf_score / (lgbm_score + rf_score)
+
+    lgbm_predictions = ensemble_lgbm()
+    rf_predictions = pd.read_csv(
+        project_root / "data/Predictions/test_rf_baseline_5fold_seed2022.csv"
+    )
+
+    final_predictions = lgbm_predictions.merge(
+        rf_predictions, on="customer_ID", how="inner", suffixes=("_lgbm", "_rf")
+    )
+    final_predictions["prediction"] = (
+        final_predictions["prediction_lgbm"] * lgbm_weight
+        + final_predictions["prediction_rf"] * rf_weight
+    )
+    return final_predictions[["customer_ID", "prediction"]]
+
+
 if __name__ == "__main__":
     new_predictions = ensemble()
     new_predictions.to_csv(
-        project_root / "data/Predictions/test_lgbm_dart_baseline_5fold_ensemble.csv", index=False
+        project_root / "data/Predictions/test_ensemble.csv", index=False
     )
